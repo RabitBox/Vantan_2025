7f3a196d9644b20ddfffaada70e4ae4cd3384168
G.Takakura <gixa.aturakka+gitlab@gmail.com>
2025-05-17 18:22:05 +0900

    Update Main.cpp

--- OpenSiv3D_0.6.161/Main.cpp
+++ OpenSiv3D_0.6.161/Main.cpp
@@ -6,12 +6,25 @@ namespace {
 	constexpr int MAX		= Y_COUNT * X_COUNT;
 }
 
+enum class ObjectType {
+	Ball,
+	Brock,
+	Paddle,
+};
+
 class GameObject {
+private:
+	ObjectType _type;
+
 public:
 	virtual void draw() = 0;
 	virtual void update() = 0;
+	virtual bool checkIntersects(GameObject* obj) { return false; }
 
+	GameObject(ObjectType type) : _type(type) {}
 	~GameObject() {}
+
+	bool matches(ObjectType type) const noexcept { return _type == type; }
 };
 
 class Ball final : public GameObject {
@@ -42,7 +55,8 @@ public:
 	}
 
 	Ball(Circle shape) :
-		_shape(shape)
+		GameObject(ObjectType::Ball)
+		, _shape(shape)
 		, _velocity(Vec2{ 0, -SPEED }) {
 	}
 	~Ball() {}
@@ -58,8 +72,14 @@ public:
 	void draw() override { _shape.stretched(-1).draw(HSV{ _shape.y - 40 }); }
 	void update() override {}
 
-	Brock() : _shape(Rect{0,0,SIZE}) {}
-	Brock(Rect shape) : _shape(shape) {}
+	Brock() :
+		GameObject(ObjectType::Brock)
+		, _shape(Rect{0,0,SIZE}) {}
+	Brock(int x, int y) :
+		GameObject(ObjectType::Brock)
+		, _shape(Rect{ 0,0,SIZE }) {
+		initPos(x, y);
+	}
 	virtual ~Brock() {}
 
 	void initPos(int x, int y) {
@@ -67,7 +87,12 @@ public:
 		_shape.y = 60 + y * SIZE.y;
 	}
 
-	bool checkIntersects(Ball* ball) {
+	bool checkIntersects(GameObject* obj) override {
+		if (!obj->matches(ObjectType::Ball)) {
+			return false;
+		}
+
+		auto ball = static_cast<Ball*> (obj);
 		if (_shape.intersects(ball->_shape)) {
 			if (_shape.bottom().intersects(ball->_shape) || _shape.top().intersects(ball->_shape))
 			{
@@ -84,7 +109,7 @@ public:
 	}
 };
 
-class Paddle final	: private GameObject {
+class Paddle final	: public GameObject {
 private:
 	Rect _shape;
 
@@ -94,31 +119,41 @@ public:
 		_shape.x = Cursor::Pos().x;
 	}
 
-	Paddle(Rect shape) : _shape(shape) {}
+	Paddle(Rect shape) :
+		GameObject(ObjectType::Paddle)
+		, _shape(shape) {}
 	~Paddle() {}
 
-	void checkIntersects(Ball* ball) {
+	bool checkIntersects(GameObject* obj) override {
+		if ( !obj->matches(ObjectType::Ball) ) {
+			return false;
+		}
+
+		auto ball = static_cast<Ball*> (obj);
 		if (_shape.intersects(ball->_shape)) {
 			ball->_velocity = Vec2{
 				(ball->_shape.x - _shape.center().x) * 10,
 				-ball->_velocity.y
 			}.setLength(ball->SPEED);
+			return true;
 		}
+		return false;
 	}
 };
 
 void Main()
 {
-	Ball* pBall = new Ball( Circle{ 400, 400, 8 } );
-	Paddle* pPaddle = new Paddle( Rect{ Arg::center(100, 500), 60, 10 } );
-	Brock brocks[MAX];
+	GameObject* pBall = new Ball(Circle{ 400, 400, 8 });
+	GameObject* pPaddle = new Paddle(Rect{ Arg::center(100, 500), 60, 10 });
+	GameObject* brocks[MAX] { nullptr };
 
 	for (int y = 0; y < Y_COUNT; ++y) {
 		for (int x = 0; x < X_COUNT; ++x) {
 			int index = (y * X_COUNT) + x;
-			brocks[index].initPos(x,y);
+			brocks[index] = new Brock(x, y);
 		}
 	}
+
 
 	while (System::Update())
 	{
@@ -126,22 +161,19 @@ void Main()
 		pPaddle->update();
 
 		pPaddle->checkIntersects( pBall );
-		for (int index = 0; index < sizeof(brocks) / sizeof(brocks[0]); index++) {
-			if (brocks[index].checkIntersects( pBall )) {
+		for (int index = 0; index < MAX; index++) {
+			if (brocks[index]->checkIntersects( pBall )) {
 				break;
 			}
 		}
 
-		// マウスカーソルを非表示にする | Hide the mouse cursor
-		Cursor::RequestStyle(CursorStyle::Hidden);
-
 		for (int i = 0; i < MAX; i++) {
-			brocks[i].draw();
+			brocks[i]->draw();
 		}
+
 		pBall->draw();
 		pPaddle->draw();
 	}
-
 	delete pBall;
 	delete pPaddle;
 }
